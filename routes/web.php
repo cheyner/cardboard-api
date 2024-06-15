549bddec4aba50c618255beadb39f42f28b14946
Christopher-Law <christopher-law@live.com>
2024-06-14 22:37:54 -0400

    docs controller and route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\DocumentationController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/docs', DocumentationController::class)->name('docs');
