338a87640fb71d6187de7f87fd492f6c532bb254
Christopher-Law <christopher-law@live.com>
2024-06-15 07:11:18 -0400

    add support the prooject link on docs

--- resources/views/documentation/index.blade.php
+++ resources/views/documentation/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 
 <section class="flex">
-    <aside class="bg-gray-100 w-1/6 p-8">
+    <aside class="bg-gray-100 w-1/6 p-8 hidden md:block">
         <h2 class="font-bold text-2xl">Reference</h2>
         <ul>
             <li>
@@ -42,7 +42,10 @@
         </ul>
     </aside>
     <div class="w-5/6 p-8">
-        <h1 class="text-4xl font-bold mb-8">Cardboard API Documentation</h1>
+        <div class="flex flex-col md:flex-row justify-between items-center">
+            <h1 class="text-4xl font-bold mb-8">Cardboard API Documentation</h1>
+            <a target="_blank" href="https://www.patreon.com/DeathLotusLabs" class="bg-orange-400 w-max whitespace-nowrap text-white font-bold rounded shadow px-3 py-2">Support the Project</a>
+        </div>
 
         <div class="flex flex-col space-y-8">
             <section class="space-y-4" id="authentication">
