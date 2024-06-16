99c5be62674df62bb47d2160521e153c76253da4
Christopher-Law <christopher-law@live.com>
2024-06-16 08:25:03 -0400

    fix schedule

--- routes/console.php
+++ routes/console.php
@@ -4,7 +4,6 @@ use App\Console\Commands\ImportScryfall;
 use Illuminate\Support\Facades\Schedule;
 
 Schedule::command(ImportScryfall::class)
-    ->purpose('Import Prices from Scryfall')
     ->daily()
     ->at('24:00')
     ->runInBackground();
